4a59b875836eb0693cabec4baaf50312a6e0c5ea
PunitChawla <chawlapunit79@gmail.com>
2024-07-25 11:30:09 +0530

    fix middleware token bug

--- Backend/middleware/middleware.js
+++ Backend/middleware/middleware.js
@@ -1,12 +1,12 @@
 const {JWT_SECRET } = require("../config")
 const jwt = require("jsonwebtoken")
 const authmiddleware  = (req, res , next) =>{
-    // const authheader = req.headers.authorization;
-    // if(!authheader || !authheader.startsWith('Bearer ')){
-    //     return res.status(403).json({msg : "token to shi de "});
-    // }
-        // const token = authheader.split(' ')[1]
-        const token = req.headers.authorization;
+    const authheader = req.headers.authorization;
+    if(!authheader ){
+        return res.status(411).json({msg : "token to shi de "});
+    }
+        const token = authheader.split(' ')[1]
+        console.log(token);
         try {
             const decode = jwt.verify(token, JWT_SECRET)
             console.log(decode);
@@ -19,6 +19,7 @@ const authmiddleware  = (req, res , next) =>{
                 return res.status(411).json({ msg  : "auth fail "});
             }
         } catch (error) {
+            console.log(error);
             return res.status(411).json({ msg  : "auth fail without try"});
         }
 }
