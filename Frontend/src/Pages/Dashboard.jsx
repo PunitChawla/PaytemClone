7a9f282142fc7f97c894b876ee69f67ef5fda0d4
PunitChawla <chawlapunit79@gmail.com>
2024-07-27 22:58:08 +0530

    error correction

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -1,21 +1,14 @@
-import { useState } from "react"
+
 import { Appbar } from "../Components/Appbar"
 import { Balance } from "../Components/Balance"
 import { Users } from "../Components/Users"
-import axios from "axios"
 export const Dashboard = async () =>{
-    const [value,setValue] = useState(0);
-   const response = await axios.get('http://localhost:3000/api/v1/account/balance', {
-        headers:{
-          Authorization : "Bearer "+localStorage.getItem("token")
-        }
-      })
 
       setValue(response.data.value)
     return<>
     <div>
      <Appbar/>
-     <Balance value={value} />
+     <Balance value={"10000"}/>
      <Users/>
     </div>
     </>
